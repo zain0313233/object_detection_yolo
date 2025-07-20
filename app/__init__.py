bf3007d4b48c3801d2469ee5652916ae7cb4bb18
zain0313233 <zain0313233@gmail.com>
2025-07-20 13:07:14 +0500

    small changes

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,jsonify,request
-
-app= Flask(__name__)
-@app.route('/')
-def home():
-    return jsonify({"message": "Welcome to the Flask app!"})
-if __name__ == '__main__':
-    app.run(debug=True)
